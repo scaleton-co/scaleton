765969267fb3b339daa3593a16da36dfa57bb389
Nick Nekilov <97469315+NickNekilov@users.noreply.github.com>
2022-05-23 02:27:17 +0200

    fix nfts endpoint address

--- src/modules/nfts/api/getNftItems.ts
+++ src/modules/nfts/api/getNftItems.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { NftItem } from '../types/NftItem';
 
 export async function getNftItems(address: string, network: string): Promise<NftItem[]> {
-  const { data: nftItems } = await axios.get('/api/v1/nfts', {
+  const { data: nftItems } = await axios.get('/.netlify/functions/nfts', {
     params: {
       address,
       network,
