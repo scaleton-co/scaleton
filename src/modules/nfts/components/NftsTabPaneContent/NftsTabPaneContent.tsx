89012f599996872245dcaf16cb9d2f71bf4843ae
Nick Nekilov <97469315+NickNekilov@users.noreply.github.com>
2022-05-23 09:59:24 +0200

    preview NFT dialog

--- src/modules/nfts/components/NftsTabPaneContent/NftsTabPaneContent.tsx
+++ src/modules/nfts/components/NftsTabPaneContent/NftsTabPaneContent.tsx
@@ -1,17 +1,25 @@
-import { Button, Card, Col, Empty, PageHeader, Row, Skeleton } from 'antd';
-import React, { useCallback, useEffect, useState } from 'react';
+import { Button, Card, Col, Descriptions, Empty, Modal, PageHeader, Row, Skeleton } from 'antd';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { isMobile } from 'react-device-detect';
 import { getNftItems } from '../../api/getNftItems';
 import { SquareImage } from '../SquareImage/SquareImage';
 import './NftsTabPaneContent.scss';
 import { IS_TESTNET } from '../../../ton/network';
+import type { NftItem } from '../../types/NftItem';
 
 interface NftsTabPaneContentProps {
   account: string;
 }
 
 export function NftsTabPaneContent({ account }: NftsTabPaneContentProps) {
-  const [assets, setAssets] = useState<any[]>([]);
+  const [assets, setAssets] = useState<NftItem[]>([]);
   const [isLoading, setLoading] = useState(true);
+
+  const [previewAssetAddress, setPreviewAssetAddress] = useState<string|null>(null);
+  const previewAsset = useMemo<NftItem|null>(
+    () => assets.find(asset => asset.address === previewAssetAddress) ?? null,
+    [assets, previewAssetAddress],
+  );
 
   const fetchAssets = useCallback(
     () => {
@@ -56,6 +64,7 @@ export function NftsTabPaneContent({ account }: NftsTabPaneContentProps) {
             <Col key={assetIndex} className="gutter-row" span={8}>
               <Card
                 hoverable
+                onClick={() => setPreviewAssetAddress(asset.address)}
                 cover={
                   <SquareImage
                     src={asset.metadata?.image!}
@@ -77,6 +86,40 @@ export function NftsTabPaneContent({ account }: NftsTabPaneContentProps) {
             description="No any NFTs available."
           />
         </div>
+      )}
+
+      {previewAsset && (
+        <Modal
+          title={previewAsset?.metadata?.name!}
+          onCancel={() => setPreviewAssetAddress(null)}
+          footer={null}
+          centered={isMobile}
+          bodyStyle={{ padding: 16 }}
+          visible
+        >
+          <div>
+            <SquareImage
+              src={previewAsset.metadata?.image!}
+              alt={previewAsset.metadata?.name ?? 'N/A'}
+            />
+          </div>
+
+          <p>
+            <div className="ant-descriptions-header">
+              <div className="ant-descriptions-title">About Collection</div>
+            </div>
+
+            <span>{previewAsset.metadata?.description}</span>
+          </p>
+
+          <p>
+            <Descriptions title="Attributes" size="small">
+              {previewAsset.metadata?.attributes?.map(attribute => (
+                <Descriptions.Item label={attribute.trait_type}>{attribute.value}</Descriptions.Item>
+              ))}
+            </Descriptions>
+          </p>
+        </Modal>
       )}
     </div>
   );
--- src/modules/ton/network.ts
+++ src/modules/ton/network.ts
@@ -1,1 +1,1 @@
-export const IS_TESTNET = ['localhost:3000', 'localhost:8888', 'testnet.scaleton.co'].includes(document.location.host);
+export const IS_TESTNET = !['localhost:3000', 'localhost:8888', 'testnet.scaleton.co'].includes(document.location.host);
