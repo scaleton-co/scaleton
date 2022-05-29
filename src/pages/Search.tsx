3da237ddf92627c5c058f8d42150b19c6ea49093
Nick Nekilov <97469315+NickNekilov@users.noreply.github.com>
2022-05-29 22:54:19 +0200

    redirect to assets page on login

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { SearchView } from '../modules/search/views/SearchView/SearchView';
+import { selectWalletAddress } from '../modules/wallets/common/selectors/selectWalletAddress';
+import { useAppSelector } from '../hooks';
 
 export function Search() {
+  const navigate = useNavigate();
+  const walletAddress = useAppSelector(selectWalletAddress);
+  const [initialWalletAddress] = useState(walletAddress);
+
+  useEffect(
+    () => {
+      if (!walletAddress) return;
+      if (initialWalletAddress) return;
+
+      navigate(`/${walletAddress}/assets`);
+    },
+    [navigate, walletAddress],
+  );
+
   return <SearchView />;
 }
